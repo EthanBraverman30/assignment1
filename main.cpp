072ff4ae72888bf3fc64e094612a3a6a906bfc00
Ethan Braverman <ethanbraver@gmail.com>
2026-06-24 20:33:13 -0700

    completed#

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+    int favnumber;
+    std::cout << "Enter your favorite number: ";
+    std::cin >> favnumber;
+    std::cout << "My favorite number is also " << favnumber << ", what a coincidence!" << std::endl;
+    std::cout << "My name is Ethan." << std::endl;
+    std::cout << "My goal in life is to be content, happy and build great relationships with people." << std::endl;
+    std::cout << "My current interests are programming, basketball, the world cup, and AI." << std::endl;
+    std::cout << "My dream is to be successful in my career and do meaningful work and also travel a lot." << std::endl;
+    std::cout << "In the next year, I see myself just finishing my freshman year at UCSB and hopefully working as an intern" << std::endl;
+    std::cout << "In the next 3 years, I see myself graduating from UCSB and starting a job in the bay area." << std::endl;
+    std::cout << "In the next 5 years, I see myself having a successful career and making a positive impact in my community." << std::endl;
+    std::cout << "In the next 7 years, hopefully I am succesful in my career and doing meaningful work and maybe bought a house." << std::endl;
+    return 0;
+}
